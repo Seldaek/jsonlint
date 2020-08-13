443f2855f75cfcbbcfb56044cf942f0909c581bf
Markus Staab <maggus.staab@googlemail.com>
2020-08-13 20:20:38 +0200

    Added more precise doc

--- src/Seld/JsonLint/ParsingException.php
+++ src/Seld/JsonLint/ParsingException.php
@@ -17,7 +17,7 @@ class ParsingException extends \Exception
 
     /**
      * @param string $message
-     * @psalm-param array{text?: string, token?: string, line?: int, loc?: array, expected?: array} $details
+     * @psalm-param array{text?: string, token?: string, line?: int, loc?: array{first_line: int, first_column: int, last_line: int, last_column: int}, expected?: string[]} $details
      */
     public function __construct($message, $details = array())
     {
@@ -26,7 +26,7 @@ class ParsingException extends \Exception
     }
 
     /**
-     * @psalm-return array{text?: string, token?: string, line?: int, loc?: array, expected?: array}
+     * @psalm-return array{text?: string, token?: string, line?: int, loc?: array{first_line: int, first_column: int, last_line: int, last_column: int}, expected?: string[]}
      */
     public function getDetails()
     {
