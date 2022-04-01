4211420d25eba80712bff236a98960ef68b866b7
Jordi Boggiano <j.boggiano@seld.be>
2022-04-01 15:37:23 +0200

    Update to latest phpstan

--- src/Seld/JsonLint/DuplicateKeyException.php
+++ src/Seld/JsonLint/DuplicateKeyException.php
@@ -14,6 +14,7 @@ namespace Seld\JsonLint;
 class DuplicateKeyException extends ParsingException
 {
     /**
+     * @phpstan-ignore-next-line
      * @var array{key: string, line: int}
      */
     protected $details;
